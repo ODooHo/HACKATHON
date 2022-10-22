72dacc2229eb3d3a2dfc94c63aa887a0f2ed8610
engh0205@naver.com <engh0205@naver.com>
2022-10-22 22:10:55 +0900

    DAICS

--- sender/Check_90.js
+++ sender/Check_90.js
@@ -1,4 +1,3 @@
-// Check_90.js
 //const spawn = require('child_process').spawn;
 //const result = spawn('python', ['print.py'));
 //js에서 파이썬 코드 실행
@@ -11,7 +10,6 @@ const result_label = document.getElementById("result_label");
 let pose_status = 2;
 let keep_time = [0, 0, 0];
 let result_message = "";
-//webcam을 enable하는 코드
 navigator.mediaDevices.getUserMedia({video: true, audio: false}).then(function (stream) {
     video.srcObject = stream;
 });
@@ -20,49 +18,46 @@ navigator.mediaDevices.getUserMedia({video: true, audio: false}).then(function (
 posenet.load().then((model) => {
     // 이곳의 model과 아래 predict의 model은 같아야 한다.
     video.onloadeddata = (e) => {
-        //비디오가 load된 다음에 predict하도록. (안하면 콘솔에 에러뜸)
         predict();
     };
 
     function predict() {
-        //frame이 들어올 때마다 estimate를 해야하니 함수화 시킴
+
         model.estimateSinglePose(video).then((pose) => {
-            canvas.width = video.width; //캔버스와 비디오의 크기를 일치시킴
+            canvas.width = video.width;
             canvas.height = video.height;
             drawKeypoints(pose.keypoints, 0.6, context);
             drawSkeleton(pose.keypoints, 0.6, context);
 
             find_state(pose);
         });
-        requestAnimationFrame(predict); //frame이 들어올 때마다 재귀호출
+        requestAnimationFrame(predict); 
     }
 });
 function sleep(ms) {
     const wakeUpTime = Date.now() + ms;
     while (Date.now() < wakeUpTime) {}
 }
-/* Timer */
 let count_time = setInterval(function () {
     if (keep_time[pose_status] == 0) {
-        //다른 모션에서 바뀌어 들어옴
         keep_time[0] = keep_time[1] = keep_time[2] = 0;
         keep_time[pose_status]++;
     } else {
         if (pose_status == 0){
-            window.parent.postMessage({message: `왼쪽 팔을 ${keep_time[pose_status]}초 뻗었습니다..`}, "*");
+            window.parent.postMessage({message: `Left arm ${keep_time[pose_status]}times detecteds.`}, "*");
             //window.alert("left");
             const spawn = require('child_process').spawn;
             const result = spawn("python3", ['send_left.py']);
             
         }
         else if (pose_status == 1){
-            window.parent.postMessage({message: `오른쪽 팔을 ${keep_time[pose_status]}초 뻗었습니다.`}, "*");
+            window.parent.postMessage({message: `Right arm ${keep_time[pose_status]}times detected.`}, "*");
             //window.alert("right");
             const spawn = require('child_process').spawn;
             const result = spawn("python3", ['send_right.py']);
         }
         else if(pose_status == 2){ 
-            window.parent.postMessage({message: `기본 자세입니다.`}, "*");
+            window.parent.postMessage({message: `default`}, "*");
             }
 
         if (pose_status != 2 && keep_time[pose_status] == 5) {
@@ -74,11 +69,10 @@ let count_time = setInterval(function () {
             clearInterval(count_time);
             window.parent.postMessage({message: result_message}, "*");
         }
-        keep_time[pose_status]++; //시간은 항상 세고 있다.
+        keep_time[pose_status]++; 
     }
 }, 1000);
 
-/* 자세 검출 함수들 */
 function find_state(pose) {
     if (!left_check(pose) && !right_check(pose)) {
         pose_status = 2;
@@ -88,42 +82,6 @@ function find_state(pose) {
         pose_status = 1;
     }
 }
-
-/*function check_HandsUp(pose) {
-    head = pose.keypoints[0].position; //머리(코)
-
-    rw = pose.keypoints[10].position; //오른쪽 손목
-    re = pose.keypoints[8].position; //오른쪽 팔꿈치
-    rs = pose.keypoints[6].position; //오른쪽 어깨
-
-    lw = pose.keypoints[9].position; //왼쪽 손목
-    le = pose.keypoints[7].position; //왼쪽 팔꿈치
-    ls = pose.keypoints[5].position; //왼쪽 어깨
-
-    //팔꿈치가 어깨보다 높을 것, 양 팔꿈치 사이에 머리가 위치할 것
-    if (re.y < rs.y && le.y < ls.y && re.x < head.x && head.x < le.x) {
-        //양쪽 손목 중, 어느 하나라도 머리보다는 위에 위치할 것
-        if (rw.y < head.y || lw.y < head.y) {
-            return true;
-        } else {
-            return false;
-        }
-    } else {
-        return false;
-    }
-}*/
-
-/*function check_O(pose) {
-    rw = pose.keypoints[10].position; //오른쪽 손목
-    re = pose.keypoints[8].position; //오른쪽 팔꿈치
-    lw = pose.keypoints[9].position; //왼쪽 손목
-    le = pose.keypoints[7].position; //왼쪽 팔꿈치
-    if (check_HandsUp(pose) && ((re.x < rw.x && rw.y < re.y) || (le.x > lw.x && le.y > lw.y))) {
-        return true;
-    } else {
-        return false;
-    }
-}*/
 
 function left_check(pose){
     head = pose.keypoints[0].position;
@@ -152,41 +110,7 @@ function right_check(pose){
     }
 }
 
-/*function check_X(pose) {
-    head = pose.keypoints[0].position; //머리(코)
-    rw = pose.keypoints[10].position; //오른쪽 손목
-    re = pose.keypoints[8].position; //오른쪽 팔꿈치
-    rs = pose.keypoints[6].position; //오른쪽 어깨
-    lw = pose.keypoints[9].position; //왼쪽 손목
-    le = pose.keypoints[7].position; //왼쪽 팔꿈치
-    ls = pose.keypoints[5].position; //왼쪽 어깨
-    b = pose.keypoints[12].position; //body(오른쪽 골반)
-    //골반보다 팔꿈치가 위쪽에 위치, 팔꿈치보다 손목이 위쪽에 위치, 손목보다 머리가 위쪽에 위치
-    if (b.y > le.y && b.y > re.y && le.y > lw.y && re.y > rw.y && lw.y > head.y && rw.y > head.y) {
-        //어깨 안쪽으로 손목이 위치
-        if (rs.x < rw.x || lw.x < ls.x) {
-            r_gradient = -1;
-            l_gradient = 1;
-            if (rw.x - re.x != 0) { //손목값에서 팔꿈치 값을 뺀다(x표에서 음의 좌표 = 오른쪽 팔의 움직임 고려)
-                r_gradient = (rw.y - re.y) / (rw.x - re.x);
-            }
-            if (lw.x - le.x != 0) { //손목값에서 팔꿈치 값을 뺀다(x표에서 양의 좌표 = 왼쪽 팔의 움직임 고려)
-                l_gradient = (lw.y - le.y) / (lw.x - le.x);
-            }
-            if (r_gradient < 0 || l_gradient > 0) { //둘중 하나라도 만족한다면, 정답으로 가정
-                return true;
-            } else {
-                return false;
-            }
-        } else {
-            return false;
-        }
-    } else {
-        return false;
-    }
-}*/
 
-//tensorflow에서 제공하는 js 파트
 const color = "aqua";
 const boundingBoxColor = "red";
 const lineWidth = 2;
